fc8879c977a5cf1665244979c63e4e3f06401a38
Amber-222 <ST10287325@vcconnect.edu.za>
2025-08-14 21:25:11 +0200

    api imports added to gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //api stuff
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
 }
---
+++ app/src/main/java/com/example/prjlibraryjetpack/apiService.kt
@@ -0,0 +1,13 @@
+package com.example.prjlibraryjetpack
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+class apiService {
+}
