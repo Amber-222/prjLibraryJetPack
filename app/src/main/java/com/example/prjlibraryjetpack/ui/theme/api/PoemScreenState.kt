57bbda4079ef144e7db509e9700f9656330b3235
jcvheerden <ST10316473@vcconnect.edu.za>
2025-08-15 22:15:47 +0200

    FIxed the PoemResponse issue Comment in PoemScreenState
    :)

--- app/src/main/java/com/example/prjlibraryjetpack/ui/theme/api/PoemScreenState.kt
+++ app/src/main/java/com/example/prjlibraryjetpack/ui/theme/api/PoemScreenState.kt
@@ -3,7 +3,7 @@ package com.example.prjlibraryjetpack.ui.theme.api
 import com.example.prjlibraryjetpack.PoemResponse
 
 data class PoemScreenState(
-    val poem: PoemResponse? = null, //store the poem object (Should be optional in case no poems have been created yet, so this is then where "Create new poem" in somewhere is used so that if a poem doesn't exist (like my will to live) it will be like it is okay, we showing that there is nada and you can add later. <3
+    val poem: List<PoemResponse> = emptyList(), //store the poem object (Should be empty in case no poems have been created yet, so this is then where "Create new poem" in somewhere is used so that if a poem doesn't exist (like my will to live) it will be like it is okay, we showing that there is nada and you can add later. <3
     val isLoading: Boolean = false, //tracks whether a response has been provided yet
     val error: String? = null //stores any errors that occur
 )
